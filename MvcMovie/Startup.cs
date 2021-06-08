42cced41dbf39cfe22addb4cfb271754aa2d0e2b
stepan4ukpolina <stepan4uk.polina@gmail.con>
2021-06-08 20:32:15 +0300

    Работа с БД

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MvcMovie
 {
@@ -24,6 +26,9 @@ namespace MvcMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
